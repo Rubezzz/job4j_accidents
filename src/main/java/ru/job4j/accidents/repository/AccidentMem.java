803e7c5484d8406a23420053e821ace14ff84214
Rubezzz <inigor1@mail.ru>
2024-03-19 23:17:18 +0700

    5. Form с агрегационными объектами [#305523]

--- src/main/java/ru/job4j/accidents/repository/AccidentMem.java
+++ src/main/java/ru/job4j/accidents/repository/AccidentMem.java
@@ -2,6 +2,7 @@ package ru.job4j.accidents.repository;
 
 import org.springframework.stereotype.Repository;
 import ru.job4j.accidents.model.Accident;
+import ru.job4j.accidents.model.AccidentType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,10 +15,14 @@ public class AccidentMem {
 
     private Map<Integer, Accident> accidents = new ConcurrentHashMap<>();
     private AtomicInteger index = new AtomicInteger(0);
+    private List<AccidentType> types = new ArrayList<>();
 
     public AccidentMem() {
-        create(new Accident(0, "ДТП", "Дтп на ул.Ленина", "Ленина, 12"));
-        create(new Accident(0, "Парковка", "Неправильная парковка", "Космонавтов, 143"));
+        types.add(new AccidentType(1, "Две машины"));
+        types.add(new AccidentType(2, "Машина и человек"));
+        types.add(new AccidentType(3, "Машина и велосипед"));
+        create(new Accident(0, "ДТП", "Дтп на ул.Ленина", "Ленина, 12", types.get(1)));
+        create(new Accident(0, "Парковка", "Неправильная парковка", "Космонавтов, 143", types.get(1)));
     }
 
     public Accident findById(int id) {
@@ -30,6 +35,7 @@ public class AccidentMem {
 
     public void create(Accident accident) {
         accident.setId(index.getAndIncrement());
+        accident.setType(types.get(accident.getType().getId()));
         accidents.put(accident.getId(), accident);
     }
 
