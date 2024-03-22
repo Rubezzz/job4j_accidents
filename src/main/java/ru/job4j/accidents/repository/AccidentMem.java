c6760975b2a212b85d65018168ab5e454262a98a
Rubezzz <inigor1@mail.ru>
2024-03-22 21:18:54 +0700

    4. Form с композиционным объектом [#305522 #486899]

--- src/main/java/ru/job4j/accidents/repository/AccidentMem.java
+++ src/main/java/ru/job4j/accidents/repository/AccidentMem.java
@@ -1,8 +1,8 @@
 package ru.job4j.accidents.repository;
 
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Repository;
 import ru.job4j.accidents.model.Accident;
-import ru.job4j.accidents.model.AccidentType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,17 +12,11 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
+@NoArgsConstructor
 public class AccidentMem {
 
     private Map<Integer, Accident> accidents = new ConcurrentHashMap<>();
     private AtomicInteger index = new AtomicInteger(0);
-    private List<AccidentType> types;
-
-    public AccidentMem(AccidentTypeMem accidentTypeMem) {
-        types = accidentTypeMem.findAll();
-        create(new Accident(0, "ДТП", "Дтп на ул.Ленина", "Ленина, 12", types.get(0)));
-        create(new Accident(0, "Парковка", "Неправильная парковка", "Космонавтов, 143", types.get(0)));
-    }
 
     public Optional<Accident> findById(int id) {
         return Optional.ofNullable(accidents.get(id));
@@ -34,7 +28,6 @@ public class AccidentMem {
 
     public void create(Accident accident) {
         accident.setId(index.getAndIncrement());
-        accident.setType(types.get(accident.getType().getId()));
         accidents.put(accident.getId(), accident);
     }
 
--- src/main/java/ru/job4j/accidents/service/AccidentService.java
+++ src/main/java/ru/job4j/accidents/service/AccidentService.java
@@ -1,32 +1,53 @@
 package ru.job4j.accidents.service;
 
-import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.job4j.accidents.model.Accident;
+import ru.job4j.accidents.model.AccidentType;
 import ru.job4j.accidents.repository.AccidentMem;
+import ru.job4j.accidents.repository.AccidentTypeMem;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-@AllArgsConstructor
 public class AccidentService {
 
-    private AccidentMem repository;
+    private AccidentMem accidentRepository;
+    private AccidentTypeMem accidentTypeRepository;
+
+    public AccidentService(AccidentMem accidentMem, AccidentTypeMem accidentTypeMem) {
+        this.accidentRepository = accidentMem;
+        this.accidentTypeRepository = accidentTypeMem;
+        initAccidents();
+    }
+
+    private void initAccidents() {
+        create(new Accident(0, "ДТП",
+                "Дтп на ул.Ленина",
+                "Ленина, 12",
+                accidentTypeRepository.findById(0).get()));
+        create(new Accident(0,
+                "Парковка",
+                "Неправильная парковка",
+                "Космонавтов, 143",
+                accidentTypeRepository.findById(0).get()));
+    }
 
     public Optional<Accident> findById(int id) {
-        return repository.findById(id);
+        return accidentRepository.findById(id);
     }
 
     public List<Accident> findAll() {
-        return repository.findAll();
+        return accidentRepository.findAll();
     }
 
     public void create(Accident accident) {
-        repository.create(accident);
+        Optional<AccidentType> accidentType = accidentTypeRepository.findById(accident.getType().getId());
+        accidentType.ifPresent(accident::setType);
+        accidentRepository.create(accident);
     }
 
     public void save(Accident accident) {
-        repository.save(accident);
+        accidentRepository.save(accident);
     }
 }
